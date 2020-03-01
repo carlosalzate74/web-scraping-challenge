978b70ae5cafe62186f87dea12c0ec5ec3483672
Carlos Alzate <carlos-alzate@hotmail.com>
2020-03-01 15:41:53 -0600

    Fixing url for JPL Featured Image

--- scrape_mars.py
+++ scrape_mars.py
@@ -6,7 +6,9 @@ import black
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import pymongo
+import re
 
 def read_scrape():
     conn = "mongodb://localhost:27017"
@@ -43,7 +45,11 @@ def scrape():
     # Define NASA News url
     NASA_URL = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 
-    driver = webdriver.Firefox()
+    # Setting up Firefox
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+
     driver.get(NASA_URL)
     driver.implicitly_wait(10)
     nasa_html = driver.page_source
@@ -67,17 +73,28 @@ def scrape():
         ]
     ]
 
-    # Extract News Image
-    driver.find_elements_by_class_name("list_image")[0].click()
-    image_url = driver.find_element_by_id("main_image").get_attribute("src")
-
     driver.close()
+
+    ## Define Feature Image Url
+    IMAGE_URL = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+
+    # Loading page
+    response = requests.get(IMAGE_URL)
+    image_url_soup = BeautifulSoup(response.text, "html.parser")
+
+    image_url = image_url_soup.find("article", {"class": "carousel_item"})["style"]
+
+    featured_image_url = re.findall("url\((.*?)\)", image_url)[0].replace("'", "")
+
+    featured_image_url = "https://www.jpl.nasa.gov" + featured_image_url
+
+    
 
     # Create the result list
     nasa_result = []
     nasa_result.append(news_title)
     nasa_result.append(news_p)
-    nasa_result.append(image_url)
+    nasa_result.append(featured_image_url)
 
 
     # # Twitter Mars Weather
@@ -115,7 +132,11 @@ def scrape():
     # Define Astrogeology url
     MARS_HEM_URL = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-    driver = webdriver.Firefox()
+    # Setting up Firefox
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+
     driver.get(MARS_HEM_URL)
     driver.implicitly_wait(10)
 
